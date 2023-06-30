61a473cb6b4239de2122fe5fa560b78caf1f4eda
DidierMatrullo <131365292+DidierMatrullo@users.noreply.github.com>
2023-06-30 18:33:56 +0200

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+// Burger menu including left nav links
+
+const leftNavLinks = document.querySelectorAll('.left-nav')
+
+
+icons.addEventListener('click', () => {
+    navbar.classList.toggle('active')
+})
+
+leftNavLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        navbar.classList.remove('active')
+    })
+})
+
+// h1 store title animation on mouseenter
+
+const store = document.querySelector('#store')
+const storeTitleToShow = document.querySelector('.storeTitle')
+
+store.addEventListener('mouseenter', (e) => {
+    storeTitleToShow.classList.add('revealStoreTitle')
+})
