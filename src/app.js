c58c125d4df6b0a9df301e1d0ab2ba6317b2fcff
DidierMatrullo <131365292+DidierMatrullo@users.noreply.github.com>
2023-09-13 14:44:14 +0200

    Add files via upload
    color button on hover modified in css

---
+++ src/app.js
@@ -0,0 +1,26 @@
+// Burger menu including left nav links
+
+const leftNavLinks = document.querySelectorAll('.left-nav')
+const icon = document.querySelector('#icons')
+const navbar = document.querySelector('#navbar');
+
+let icons;
+icon.addEventListener('click', () => {
+  navbar.classList.toggle('active')
+})
+
+leftNavLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    navbar.classList.remove('active')
+  })
+})
+
+// h1 store title animation on mouseenter
+
+const store = document.getElementById("store")
+const titleToShow = document.getElementById("store_title")
+
+store.addEventListener('mouseenter', () => {
+  titleToShow.classList.add('reveal_store_title')
+})
+
